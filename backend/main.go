31d671682754cfd59e2e0ab2f96d3caf7a6daaf7
Zuk Levinson <zuklevinson@gmail.com>
2021-09-08 18:47:08 +0300

    fix(Dockerfile): fixes issues with the dockerfiles

---
+++ backend/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
+func main () {
+	flag.Parse()
+
+	http.HandleFunc("/", serveHome)
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
