01fb37e1bfaf594e68e46352bb43147519b4f32a
Zuk Levinson <zuklevinson@gmail.com>
2021-09-08 19:50:10 +0300

    feat(main.go): added switch case for paths

--- backend/main.go
+++ backend/main.go
@@ -4,27 +4,29 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
+func serveAPI(w http.ResponseWriter, r *http.Request) {
+	switch strings.TrimLeft(r.URL.Path, "/api"){
+	case "/chatters":
+
+	case "/messages":
+
+	case "/groups":
+
+	default:
+		http.Error(w, "Uknown path", http.StatusNotFound)
 		return
 	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
 }
 
 func main () {
 	flag.Parse()
 
-	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/api", serveAPI)
 
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
