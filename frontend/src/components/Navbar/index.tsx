e0fc14ff77021c53dffa52771bae878b37178ff7
amit <amittima1234@gmail.com>
2021-09-16 23:59:09 +0300

    fix(Navbar): fix margin of logo in sidebar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,19 +5,19 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import ForumIcon from "@mui/icons-material/Forum";
 import ContactsIcon from "@mui/icons-material/Contacts";
 import LogoutIcon from "@mui/icons-material/Logout";
-import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
 import { ReactComponent as AppLogo } from "../../assets/icons/logo.svg";
 
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <div>
-        <AppLogo />
-      </div>
       <div className={styles["buttons-container"]}>
+        <div className={styles.logo}>
+          <AppLogo/>
+        </div>
         <NavbarButton title="Chats" icon={ForumIcon} />
         <NavbarButton title="Contacts" icon={ContactsIcon} />
-        <NavbarButton title="My Account" icon={AccountCircleIcon} />
+        <NavbarButton title="Account" icon={AccountCircleIcon} />
         <NavbarButton title="Settings" icon={SettingsIcon} />
       </div>
       <div className={styles["buttons-container"]}>
