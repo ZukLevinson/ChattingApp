581c26ae621728f73374363b73846969ac83622e
Zuk Levinson <zuklevinson@gmail.com>
2021-09-16 19:14:56 +0300

    feat(Chat, UserProfileBadge): added users

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -1,20 +1,67 @@
 import styles from "./index.module.css";
 import UserProfileBadge, {
   Props as UserProfileBadgeProps,
+  Status,
 } from "../UserProfileBadge";
 
-const tempCurrentChat: UserProfileBadgeProps = {
-  username: "amit",
-  status: "Online",
-};
+const tempOnlineUsers: UserProfileBadgeProps[] = [
+  {
+    username: "amit",
+    status: Status.online,
+  },
+  {
+    username: "zuk",
+    status: Status.inChat,
+  },
+  {
+    username: "bruh",
+    status: Status.offline,
+  },
+];
+const tempOfflineUsers: UserProfileBadgeProps[] = [
+  {
+    username: "amit1",
+    status: Status.offline,
+  },
+];
 
 interface Props {}
 
 export default function Chat(props: Props) {
   return (
     <div className={styles.container}>
-      <div>
-        <UserProfileBadge {...tempCurrentChat} />
+      <div className={styles.header}>
+        <div className={styles.title}>
+          <span>Private Chat</span>
+        </div>
+        <div className={styles["users-container"]}>
+          <div className={styles["users-section"] + " " + styles.actives}>
+            <div className={styles["users-section-header"]}>
+              <span>Reacently Active</span>
+            </div>
+            <div className={styles.users}>
+              {tempOnlineUsers.map((user) => (
+                <UserProfileBadge {...user} />
+              ))}
+            </div>
+          </div>
+          <div className={styles["users-section"] + " " + styles.offlines}>
+            <div className={styles["users-section-header"]}>
+              <span>Others</span>
+            </div>
+            <div className={styles.users}>
+              {tempOfflineUsers.map((user) => (
+                <UserProfileBadge {...user} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <span>hi im the chat</span>
+      </div>
+      <div className={styles.footer}>
+        <span>hi im the footer</span>
       </div>
     </div>
   );
--- frontend/src/components/UserProfileBadge/index.tsx
+++ frontend/src/components/UserProfileBadge/index.tsx
@@ -3,9 +3,15 @@ import { FunctionComponent, useState } from "react";
 // import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { ReactComponent as Avatar } from "../../assets/icons/account_circle_black_24dp.svg";
 
+export enum Status {
+  online = "Online",
+  offline = "Offline",
+  inChat = "In chat",
+}
+
 export interface Props {
   username: string;
-  status: "Online" | "Offline" | "In chat";
+  status: Status;
   icon?: FunctionComponent;
 }
 
@@ -19,10 +25,24 @@ export default function UserProfileBadge(props: Props) {
         <div className={styles.username}>
           <span>{props.username}</span>
         </div>
-        <div className={styles.status}>
+        <div
+          className={styles.status}
+          style={{ color: getColor(props.status) }}
+        >
           <span>{props.status}</span>
         </div>
       </div>
     </div>
   );
+}
+
+function getColor(status: Status) {
+  switch (status) {
+    case Status.online:
+      return "var(--online-color)";
+    case Status.offline:
+      return "var(--offline-color)";
+    case Status.inChat:
+      return "var(--in-chat-color)";
+  }
 }
