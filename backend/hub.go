e84f06d74bf7df6a31d77e961d8502db8fb3f95a
amit <amittima1234@gmail.com>
2021-09-25 00:24:49 +0300

    fix(): empty commit

--- backend/hub.go
+++ backend/hub.go
@@ -7,41 +7,41 @@ import (
 
 // The hub maintains the different channels while keeping records of the middlemans
 type Hub struct {
-	name string // Hub name
-	middlemans map[*Middleman]bool // Registered middlemans
-	broadcast chan []byte // Messages from  middlemans
-	register chan *Middleman // Register requests from middlemans
-	unregister chan *Middleman // Unregister requests from middlemans
+	name         string              // Hub name
+	middlemans   map[*Middleman]bool // Registered middlemans
+	broadcast    chan []byte         // Messages from  middlemans
+	register     chan *Middleman     // Register requests from middlemans
+	unregister   chan *Middleman     // Unregister requests from middlemans
 	allowedUsers map[string]*Middleman
 }
 
 func (h *Hub) listAllowedUsers() []string {
 	keys := make([]string, 0, len(h.allowedUsers))
 
-    for k := range h.allowedUsers {
-        keys = append(keys, k)
-    }
-	
+	for k := range h.allowedUsers {
+		keys = append(keys, k)
+	}
+
 	return keys
 }
 
 func (h *Hub) checkIfInAllowedUsers(userId string) bool {
 	allowedUsersList := h.listAllowedUsers()
-	
-    for _, val := range allowedUsersList {
+
+	for _, val := range allowedUsersList {
 		log.Println("Checking user - ", val)
-        if (val == userId) {
+		if val == userId {
 			log.Println("User", val, "is valid!")
 			return true
 		}
-    }
-	
+	}
+
 	return false
 }
 
 func (h *Hub) registerMiddleman(middleman *Middleman) error {
-	if(h.checkIfInAllowedUsers(middleman.userId)){
-		h.register<-middleman
+	if h.checkIfInAllowedUsers(middleman.userId) {
+		h.register <- middleman
 		log.Println("Registered middleman to Hub with the name - ", h.name)
 
 		return nil
@@ -49,7 +49,7 @@ func (h *Hub) registerMiddleman(middleman *Middleman) error {
 		log.Println("Middleman not allowed!")
 
 		return errors.New("UserId not allowed")
-	} 
+	}
 }
 
 // Creates new Hub
@@ -57,23 +57,23 @@ func createHub(name string, allowedUsersList []string) *Hub {
 	// Create allowed users map (while middleman is STILL UNKNOWN!)
 	allowedUsers := make(map[string]*Middleman)
 	for _, val := range allowedUsersList {
-        allowedUsers[val] = nil
-    }
+		allowedUsers[val] = nil
+	}
 
 	// Returns the pointer of the new Hub
 	return &Hub{
-		name: name,
-		broadcast: make(chan []byte),
-		register: make(chan *Middleman),
-		unregister: make(chan *Middleman),
-		middlemans: make(map[*Middleman]bool),
+		name:         name,
+		broadcast:    make(chan []byte),
+		register:     make(chan *Middleman),
+		unregister:   make(chan *Middleman),
+		middlemans:   make(map[*Middleman]bool),
 		allowedUsers: allowedUsers,
 	}
 }
 
 func (h *Hub) updateUsers(currentlyAllowedUsers []string) {
 	for _, userId := range currentlyAllowedUsers {
-		if(!h.checkIfInAllowedUsers(userId)) {
+		if !h.checkIfInAllowedUsers(userId) {
 			h.allowedUsers[userId] = nil
 		}
 	}
@@ -82,29 +82,29 @@ func (h *Hub) updateUsers(currentlyAllowedUsers []string) {
 func (h *Hub) runHub() {
 	for {
 		select {
-			// Incoming registered middleman
-			case middleman := <-h.register:
-				log.Println("Incoming registered middleman")
-				h.middlemans[middleman] = true
+		// Incoming registered middleman
+		case middleman := <-h.register:
+			log.Println("Incoming registered middleman")
+			h.middlemans[middleman] = true
 
-			// Incoming unregistered middleman
-			case middleman := <-h.unregister:
-				if _, ok := h.middlemans[middleman]; ok {
-					log.Println("Incoming unregistered middleman")
+		// Incoming unregistered middleman
+		case middleman := <-h.unregister:
+			if _, ok := h.middlemans[middleman]; ok {
+				log.Println("Incoming unregistered middleman")
+				middleman.unregister(h)
+			}
+
+		// Incoming message
+		case message := <-h.broadcast:
+			// Loops through all the middlemans
+			for middleman := range h.middlemans {
+				select {
+				case middleman.send <- message: // If the middleman's channel is active, insert the message
+					log.Println("Spreaded the message '", message, "'")
+				default:
 					middleman.unregister(h)
 				}
-
-			// Incoming message
-			case message := <- h.broadcast:
-				// Loops through all the middlemans
-				for middleman := range h.middlemans {
-					select{
-						case middleman.send <- message: // If the middleman's channel is active, insert the message
-							log.Println("Spreaded the message '",message, "'")
-						default:
-							middleman.unregister(h)
-					}
-				}
+			}
 		}
 	}
 }
--- backend/main.go
+++ backend/main.go
@@ -14,45 +14,45 @@ var addr = flag.String("addr", ":8080", "http service address")
 func serveAPI(w http.ResponseWriter, r *http.Request, hub *Hub) {
 	middleman, err := createMiddleman(hub, w, r)
 
-	if(err != nil) {
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	} else {
 		log.Println("Created middleman with userId of - ", middleman.userId)
 		registrationErr := middleman.hub.registerMiddleman(middleman)
 
-		if(registrationErr != nil) {
+		if registrationErr != nil {
 			closeMessage := websocket.FormatCloseMessage(websocket.CloseNormalClosure, registrationErr.Error())
 
 			middleman.conn.WriteMessage(websocket.CloseMessage, closeMessage)
 			middleman.conn.Close()
 
 			log.Println(registrationErr.Error())
-			} else {
+		} else {
 			go middleman.writingFromTheHubToWS()
 			go middleman.readingFromWSToHub()
 		}
 	}
 }
 
-func main () {
+func main() {
 	flag.Parse()
 
-	groupHubs := make(map[int] *Hub)
+	groupHubs := make(map[int]*Hub)
 
 	statusHub := createHub("Status", []string{"1"})
 
 	go statusHub.runHub()
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Request from ", r.URL.Path)
 
-		switch(r.URL.Path) {
+		switch r.URL.Path {
 		case "/messages":
 			queryMap := r.URL.Query()
 
 			groupId, err := strconv.Atoi(queryMap.Get("groupId"))
 
-			if(err != nil){ // Emit http error since no group with groupId was found
+			if err != nil { // Emit http error since no group with groupId was found
 				w.WriteHeader(http.StatusInternalServerError)
 				w.Write([]byte("Unknown group id!"))
 
@@ -60,32 +60,31 @@ func main () {
 			} else {
 				group, err := createGroupInstance(groupId)
 
-				if(err != nil){ // Emit http error since no group with groupId was found
+				if err != nil { // Emit http error since no group with groupId was found
 					w.WriteHeader(http.StatusInternalServerError)
 					w.Write([]byte("Cannot fetch group data!"))
-	
+
 					panic("Cannot fetch group data")
-				} 
+				}
 				group.getGroupUsers()
-					
+
 				allowedUsersIds := []string{}
 
-				for _, userInGroup := range *group.Users{
+				for _, userInGroup := range *group.Users {
 					allowedUsersIds = append(allowedUsersIds, strconv.Itoa(userInGroup.UserId))
 				}
 
-				if(groupHubs[groupId] == nil) {
+				if groupHubs[groupId] == nil {
 					groupHubs[groupId] = createHub(queryMap.Get("groupId"), allowedUsersIds)
-					log.Println("Created Hub - ",groupHubs[groupId], ", Group - ", group, ", Allowed Users - ", allowedUsersIds)
+					log.Println("Created Hub - ", groupHubs[groupId], ", Group - ", group, ", Allowed Users - ", allowedUsersIds)
 				} else {
 					groupHubs[groupId].updateUsers(allowedUsersIds)
 				}
 
-
 				go groupHubs[groupId].runHub()
 				serveAPI(w, r, groupHubs[groupId])
 			}
-		case "/statuses": 
+		case "/statuses":
 			serveAPI(w, r, statusHub)
 		default:
 			log.Panic("No available routes")
